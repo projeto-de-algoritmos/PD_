c34001b00f6c87d51fd77c035f8de568ffa04f14
joaobisi <jbisinotti@gmail.com>
2023-07-11 00:39:22 -0300

    Adicionando página inicial onde usuario insere capacidade da mochila
    Co-authored-by: pedrohelias <pedrohelias95@hotmail.com>

---
+++ knapsack_app/lib/pages/capacity_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'knapsack_page.dart';
+
+class CapacidadePage extends StatelessWidget {
+  final TextEditingController capacidadeController = TextEditingController();
+
+  CapacidadePage({super.key});
+
+  void inserirItens(BuildContext context) {
+    int capacidade = int.tryParse(capacidadeController.text) ?? 100;
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MochilaPage(capacidadeMochila: capacidade)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Capacidade da Mochila'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: capacidadeController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Capacidade da Mochila'),
+            ),
+            ElevatedButton(
+              onPressed: () => inserirItens(context),
+              child: const Text('Continuar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
